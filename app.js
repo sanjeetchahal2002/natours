b8792e4db0d64119c068faece08b2e7e51c7979b
Sanjeet Singh <sanjeetsinghchahal933@gmail.com>
2025-02-17 11:51:32 +0530

    Added cors

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.options('*', cors());
+
 // app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
